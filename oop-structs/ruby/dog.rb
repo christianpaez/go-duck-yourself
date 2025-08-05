b3d16d0a7699b1867477caa6359fe54fc1b145cc
Christian Paez <christianpaeznegron@hotmail.com>
2025-08-04 23:47:00 -0500

    Ruby OOP example

---
+++ oop-structs/ruby/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  BEST_DOG_BREEDS = ['Golden Retriever', 'Border Collie'].freeze
+  def initialize(name, breed)
+    @breed = breed
+    @name = name
+  end
+
+  def is_a_good_dog
+    if best_breed?
+      puts "#{@name} is the best dog ever, congrats!"
+    else
+      puts "#{@name} is not the best dog, sorry."
+    end
+  end
+
+  private
+
+  attr_reader :breed, :name
+  def best_breed?
+    BEST_DOG_BREEDS.include?(@breed)
+  end
+end
+
+noah = Dog.new('Noah', 'Golden Retriever')
+nina = Dog.new('Nina', 'Border Collie')
+pepe = Dog.new('Pepe', 'Pitbull')
+
+noah.is_a_good_dog
+nina.is_a_good_dog
+pepe.is_a_good_dog
+
